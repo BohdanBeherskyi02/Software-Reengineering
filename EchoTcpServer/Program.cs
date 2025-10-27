ad2bad4267ec0e63b14028874f7819d5c819ba72
BohdanBeherskyi02 <8634981@stud.kai.edu.ua>
2025-10-27 08:06:02 +0200

    Update Program.cs
    revert listner change

--- EchoTcpServer/Program.cs
+++ EchoTcpServer/Program.cs
@@ -10,7 +10,7 @@ namespace EchoServer
     public class EchoServer
     {
         private readonly int _port;
-        private required TcpListener _listener {get; init;}
+        private TcpListener _listener;
         private readonly CancellationTokenSource _cancellationTokenSource;
 
         //constuctor
